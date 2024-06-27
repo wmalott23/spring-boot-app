fdb3bf36745fe352fc05dafa1cfde6ed7da42ba5
Wesley Malott <wesley_malott@yahoo.com>
2024-06-26 21:43:29 -0700

    Completed Deep Copy Interview Question

--- tutorial/src/main/java/com/example/tutorial/MethodExample.java
+++ tutorial/src/main/java/com/example/tutorial/MethodExample.java
@@ -1,6 +1,8 @@
 package com.example.tutorial;
 
 public class MethodExample {
+    public int x;
+
     public void printStuff(int x) {
         System.out.print("MethodExample");
     }
--- tutorial/src/main/java/com/example/tutorial/controller/TestController.java
+++ tutorial/src/main/java/com/example/tutorial/controller/TestController.java
@@ -6,7 +6,18 @@ import com.example.tutorial.MethodExample;
 
 public class TestController {
     public static void main(String[] args) {
-        MethodExample e = new MethodExample();
-        e.printStuff(10);
+//  "Method Overriding example"
+//        MethodExample e = new MethodExample();
+//        e.printStuff(10);
+
+//  "Deep Copy Example"
+        MethodExample example1 = new MethodExample();
+        example1.x = 50;
+        MethodExample example2 = example1;
+        example2.x = 100;
+
+        System.out.print(example1.x);
+
+
     }
 }
